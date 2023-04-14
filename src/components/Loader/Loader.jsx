90e34210a3b92cd5a98c1ca4d7ef02600b1bb7ea
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-04-14 22:12:49 +0300

    add wrapper for loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,26 @@
 import { MagnifyingGlass } from 'react-loader-spinner';
+import styled from '@emotion/styled';
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+`;
 
 const Loader = () => {
   return (
-    <MagnifyingGlass
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="MagnifyingGlass-loading"
-      wrapperStyle={{}}
-      wrapperClass="MagnifyingGlass-wrapper"
-      glassColor="#c0efff"
-      color="#e15b64"
-    />
+    <LoaderWrapper>
+      <MagnifyingGlass
+        visible={true}
+        height="200"
+        width="200"
+        ariaLabel="MagnifyingGlass-loading"
+        wrapperStyle={{}}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor="#c0efff"
+        color="#e15b64"
+      />
+    </LoaderWrapper>
   );
 };
 
